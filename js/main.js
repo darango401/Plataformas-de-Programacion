1d45af5a8114251fc61edadea21d04836a8cfc75
Daniel Altman <daniel.arangoa@upb.com>
2020-08-02 15:07:08 -0500

    Arreglo select dinamico

--- js/main.js
+++ js/main.js
@@ -22,23 +22,23 @@ const campos = {
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "usuario":
-            validarCampo(expresiones.usuario, e.target, 'usuario');
+        validarCampo(expresiones.usuario, e.target, 'usuario');
         break;
         case "nombre":
-            validarCampo(expresiones.nombre, e.target, 'nombre');
+        validarCampo(expresiones.nombre, e.target, 'nombre');
         break;
         case "contrasena":
-            validarCampo(expresiones.contrasena, e.target, 'contrasena');
-            validarContrasena();
+        validarCampo(expresiones.contrasena, e.target, 'contrasena');
+        validarContrasena();
         break;
         case "contrasena2":
-            validarContrasena();
+        validarContrasena();
         break;
         case "correo":
-            validarCampo(expresiones.correo, e.target, 'correo');
+        validarCampo(expresiones.correo, e.target, 'correo');
         break;
         case "apellido":
-            validarCampo(expresiones.apellido, e.target, 'apellido');
+        validarCampo(expresiones.apellido, e.target, 'apellido');
         break;
     }
 }
@@ -107,65 +107,50 @@ formulario.addEventListener('submit', (e) => {
         document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
     }
 });
-function cargar() {
-    var deptos = ["AMAZONAS","ANTIOQUIA","ARAUCA","ATLANTICO","BOLIVAR","BOYACA","CALDAS","CAQUETA","CASANARE","CAUCA","CESAR","CHOCO","CORDOBA","CUNDINAMARCA","GUAINIA","GUAJIRA","GUAVIARE","HUILA","MAGDALENA","META","NORTE DE SANTANDER","NARINO","PUTUMAYO","QUINDIO","RISARALDA","SAN ANDRES","SANTANDER","SUCRE","TOLIMA","VALLE DEL CAUCA","VAUPES","VICHADA"];//Tu array de provincias
-    var select = document.getElementById("departamento"); //Seleccionamos el select
-    
-    for(var i=0; i < deptos.length; i++){ 
-        var option = document.createElement("option"); //Creamos la opcion
-        option.innerHTML = deptos[i]; //Metemos el texto en la opción
-        select.appendChild(option); //Metemos la opción en el select
-    }
-}
-cargar();
-let municipio, departamento;
-let ciudades = [];
-ciudades[0] = ["leticia|Leticia",];
-ciudades[1] = ["medellin|Medellín","envigado|Envigado","sabaneta|Sabaneta","bello|Bello"];
-ciudades[2] = ["arauca|Arauca","arauquita|Arauquita","saravena|Saravena" ];
-ciudades[3] = ["barranquilla|Barranquilla"];
-ciudades[4] = ["cartagena|Cartagena"];
-ciudades[5] = ["tunja|Tunja"];
-ciudades[6] = ["manizales|Manizales"];
-ciudades[7] = ["florencia|Florencia"];
-ciudades[8] = ["yopal|Yopal"];
-ciudades[9] = ["popayan|Popayan"];
-ciudades[10] = ["valledupar|Valledupar"];
-ciudades[11] = ["quibdo|Quibdo"];
-ciudades[12] = ["monteria|Monteria"];
-ciudades[13] = ["bogota|Bogota","bituima|Bituima","chipaque|Chipaque","ubaque|Ubaque","cota|Cota"];
-ciudades[14] = ["inirida|Inirida"];
-ciudades[15] = ["san jose del guaviare|San Jose del Guaviare"];
-ciudades[16] = ["neiva|Neiva"];
-ciudades[17] = ["rioacha|Rioacha"];
-ciudades[18] = ["santa Marta|Santa Marta","algarrobo|Algarrobo","concordia|Concordia"];
-ciudades[19] = ["villavicencio|Villavicencio"];
-ciudades[20] = ["pasto|Pasto"];
-ciudades[21] = ["cucuta|Cucuta"];
-ciudades[22] = ["mocoa|Mocoa"];
-ciudades[23] = ["armenia|Armenia"];
-ciudades[24] = ["pereira|Pereira","dosquebradas|Dosquebradas","la virginia|La Virginia","santa rosa de cabal|Santa Rosa De Cabal"];
-ciudades[25] = ["san andres|San Andres"];
-ciudades[26] = ["bucaramanga|Bucaramanga"];
-ciudades[27] = ["sincelejo|Sincelejo"];
-ciudades[28] = ["ibague|Ibague"];
-ciudades[29] = ["cali|Cali","buga|Buga","palmira|Palmira","cartago|Cartago"];
-ciudades[30] = ["mitu|Mitu"];
-ciudades[31] = ["puerto carreño|Puerto Carreño"];
-window.onload = ()=>{
-    select_departamentos = document.getElementById("departamento");
-    municipio = document.getElementById("municipio");
+function cambio(){
+    var ciudades=
+    [["Leticia"],
+    ["Medellín","Envigado","Sabaneta","Bello"],
+     ["Arauca","Arauquita","Saravena"],
+     ["Barranquilla"],
+     ["Cartagena"],
+    ["Tunja"],
+     ["Manizales"],
+     ["Florencia"],
+     ["Yopal"],
+     ["Popayan"],
+     ["Valledupar"],
+     ["Quibdo"],
+    ["Monteria"],
+     ["Bogota","Bituima","Chipaque","Ubaque","Cota"],
+    ["Inirida"],
+   ["San Jose del Guaviare"],
+   ["Neiva"],
+   ["Rioacha"],
+   ["Santa Marta","Algarrobo","Concordia"],
+   ["Villavicencio"],
+   ["Pasto"],
+   ["Cucuta"],
+   ["Mocoa"],
+   ["Armenia"],
+   ["Pereira","Dosquebradas","La Virginia","Santa Rosa De Cabal"],
+   ["San Andres"],
+   ["Bucaramanga"],
+   ["Sincelejo"],
+   ["Ibague"],
+   ["Cali","Buga","Palmira","Cartago"],
+   ["Mitu"],
+   ["Puerto Carreño"]];
+    var arreglo;
+    var select_departamentos = document.getElementById("departamento");
+    var select_municipio = document.getElementById("municipio");
+    arreglo=ciudades[select_departamentos.value];
+    select_municipio.innerHTML = "";
+    for(var i=0; i < arreglo.length; i++){ 
+        var option = document.createElement("option"); 
+        option.innerHTML = ciudades[select_departamentos.value][i];
+        select_municipio.appendChild(option); 
+    } 
 }
 
-function Cambio(){
-    let ArregloOpciones;
-    municipio.innerHTML = "";
-    ArregloOpciones=ciudades[select_departamentos.value];
-    for(var opcion in ArregloOpciones){
-        var arreglo = ArregloOpciones[opcion].split("|");
-        var nuevaOpcion = document.createElement("option");
-        nuevaOpcion.value = arreglo[0];
-        nuevaOpcion.innerHTML = arreglo[1];
-        municipio.appendChild(nuevaOpcion);
-    }
-}
+
